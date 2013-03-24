80c2cd92c080da4db9366c3cfd70b968dd304028
Shin HATTORI <toplut@gmail.com>
2013-03-24 23:38:34 +0900

    revert update

--- kernel-uksm/create_release.sh
+++ kernel-uksm/create_release.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 export ARCH=arm
-export CROSS_COMPILE=/home/pcadmin/android-misc/ics/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
+export CROSS_COMPILE=arm-cortex_a9-linux-gnueabi-
 PARA='-j5'
-MOD_PATH="../kernel.cwm/system/"
-#IMAGE="boot.img"
+MOD_PATH="../ramdisk"
+IMAGE="boot.img"
 
 make cyanogen_nypon_defconfig && \
 make $PARA oldconfig && \
@@ -12,13 +12,5 @@ make $PARA && \
 make $PARA INSTALL_MOD_PATH=$MOD_PATH modules_install  && \
 cd ../ && \
 ./makeit_p.sh && \
-cd kernel.cwm && \
-rm *.zip ; \
-rm system/lib/modules/3.0.69\+/build ; \
-rm system/lib/modules/3.0.69\+/source ; \
-zip -r distkernel.zip * && \
-signapk.jar ~/android-misc/ssl/platform.x509.pem ~/android-misc/ssl/platform.pk8 distkernel.zip distkernel-signed.zip
-#adb push distkernel-signed.zip /mnt/sdcard
-
-#fastboot flash boot $IMAGE && \
-#fastboot reboot
+fastboot flash boot $IMAGE && \
+fastboot reboot
--- makeit_p.sh
+++ makeit_p.sh
@@ -2,7 +2,7 @@
 
 # ready zImage
 rm zImage
-rm kernel.cwm/boot.elf
+#rm kernel.elf
 rm boot.img
 rm ramdisk.img
 cp kernel-uksm/arch/arm/boot/zImage zImage
@@ -12,13 +12,9 @@ cp kernel-uksm/arch/arm/boot/zImage zImage
 #cp ./kernel/net/compat-wireless/drivers/staging/cw1200/cw1200_wlan.ko ./ramdisk/lib/modules/3.0.8+/kernel/net/compat-wireless/drivers/staging/cw1200/
 
 # make ramdisk image
-cd combinedroot
-find . | cpio --quiet -H newc -o | xz -z -9 -c --check=crc32 > ../ramdisk.img
+cd ramdisk
+find . | cpio --quiet -H newc -o | lzop -9 -c > ../ramdisk.img
 cd ..
 
 # make kernel
 ./mkbootimg --cmdline 'ecc=on cachepolicy=writealloc noinitrd init=init board_id=1 logo.nologo root=/dev/ram0 rw rootwait mem=96M@0 mem_mtrace=15M@96M mem_mshared=1M@111M mem_modem=16M@112M mem=32M@128M mem_issw=1M@160M hwmem=167M@161M mem=696M@328M vmalloc=384M console=ttyAMA2 androidboot.console=ttyAMA2 androidboot.hardware=st-ericsson' --kernel zImage --ramdisk ramdisk.img --base 0x0 --output boot.img
-
-python mkelf.py -o kernel.cwm/boot.elf zImage@0x8000 ramdisk.img@0x01000000,ramdisk cmdline.txt@cmdline
-
-
